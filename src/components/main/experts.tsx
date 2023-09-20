c623a8f7d65e6b612e0365521a9ee9739e2bd2cd
0Ieg <obognov@gmail.com>
2023-09-20 23:20:27 +0300

    Minor improvements

--- src/components/main/experts.tsx
+++ src/components/main/experts.tsx
@@ -7,6 +7,23 @@ import { Information } from '../information';
 const ExpertsS = styled.section`
 height: 100dvh;
 background-color: var(--color-background);
+.statistics{
+  width: 654px;
+  height: 117px;
+  border-radius: 16px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: white;
+  .value{
+    color: var(--color-red);
+    font: 700 36px Rubik;
+  }
+  .name{
+    color: var(--color-black);
+    font: 700 11px Rubik;
+  }
+}
 .information{
   .title{}
   .description{max-width: 363px;}
@@ -16,7 +33,20 @@ export const Experts:FC = ()=>{
 return(
   <ExpertsS>
     <div className="__container">Experts</div>
-    <div className="statistics"></div>
+    <div className="statistics">
+      <div className="users">
+        <div className="value">2m</div>
+        <div className="name">Users</div>
+      </div>
+      <div className="countries">
+        <div className="value">78</div>
+        <div className="name">Countries</div>
+      </div>
+      <div className="experts">
+        <div className="value">10,000+</div>
+        <div className="name">Medical experts</div>
+      </div>
+    </div>
     <div className="video"></div>
     <div className="information">
       <Information leftTitle='Talk to' rightTitle='experts.' rightColor='blue' descript='Book appointments or submit queries into thousands of forums concerning health issues and prevention against noval Corona Virus.'/>
