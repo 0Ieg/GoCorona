932b9dc08395227288156954b93e43ea8de12e6b
0Ieg <obognov@gmail.com>
2023-09-25 23:12:45 +0300

    Layout of the section with cards

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,21 +2,47 @@ import { FC } from 'react'
 import styled from "styled-components";
 
 const CardS = styled.div`
-background-color: #fff;
 border-radius: 8px;
+width: 278px;
+height: 278px;
+padding: 30px 10px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-between;
+box-shadow: 0 0 12px -6px var(--color-blue);
+cursor: pointer;
+transition: box-shadow 0.1s linear;
+&:hover{
+  box-shadow: 0 0 10px -5px var(--color-red);
+}
 .image {
 }
+.text{
+  text-align: center;
+}
 .title {
+  font-weight: 700;
+  letter-spacing: 0.08rem;
+  margin-bottom: 8px;
 }
 .description {
+  max-width: 242px;
+  font-size: 12px;
+  color: var(--color-gray);
+  line-height: 138%;
+  text-align: center;
 }
 `
-export const Card:FC = (props)=>{
+export const Card:FC<{icon:string, title:string, description:string}> = (props)=>{
+  const {icon,title, description} = props
   return(
     <CardS>
-      <img src="" className="image" />
-      <div className="title"></div>
-      <div className="description"></div>
+      <img src={icon} className="image" />
+      <div className="text">
+        <div className="title">{title}</div>
+        <div className="description">{description}</div>
+      </div>
     </CardS>
   )
 }
--- src/components/main/application.tsx
+++ src/components/main/application.tsx
@@ -4,12 +4,21 @@ import styled from "styled-components";
 import { Information } from '../information';
 import googlePlay from '../../bll/images/googlePlay.svg'
 import appleStore from '../../bll/images/appStore.svg'
+import { Card } from '../card';
+import card1 from '../../bll/images/Card1.svg'
+import card2 from '../../bll/images/Card2.svg'
+import card3 from '../../bll/images/Card3.svg'
 
 const ApplicationS = styled.section`
 height: 100dvh;
 .information{
   .title{}
   .description{max-width: 600px;}
+}
+.cards{
+  display: flex;
+  justify-content: space-between;
+  gap: 40px;
 }
 .links{
   display: flex;
@@ -21,30 +30,23 @@ height: 100dvh;
 export const Application:FC = ()=>{
 return(
   <ApplicationS>
-    <div className="__container">Application</div>
-    <div className="information">
-      <Information leftTitle='Healthcare' rightTitle=' at your Fingertips.' leftColor='red' descript='Bringing premium healthcare features to your fingertips. User friendly mobile platform to bring healthcare to your fingertips. Signup and be a part of the new health culture.'/>
-    </div>
-    <div className="cards">
-      <div className="card">
-        <img src="" className="card__image" />
-        <div className="card__title"></div>
-        <div className="card__description"></div>
+    <div className="__container">
+      <div className="information">
+        <Information leftTitle='Healthcare' rightTitle=' at your Fingertips.' leftColor='red' descript='Bringing premium healthcare features to your fingertips. User friendly mobile platform to bring healthcare to your fingertips. Signup and be a part of the new health culture.'/>
       </div>
-      <div className="card">
-        <img src="" className="card__image" />
-        <div className="card__title"></div>
-        <div className="card__description"></div>
+      <div className="cards">
+        <Card icon={card1} title='Symptom Checker' description='Check if you are infected by COVID-19 with our Symptom Checker'/>
+        <Card icon={card2} title='24x7 Medical support' description='Consult with 10,000+ health workers about your concerns.'/>
+        <Card icon={card3} title='Conditions' description='Bringing premium healthcare features to your fingertips.'/>
       </div>
-
-    </div>
-    <div className="links">
-      <a href="https://play.google.com" target='_blank'>
-        <img src={googlePlay}/>
-      </a>
-      <a href="https://www.apple.com/ru/app-store/" target='_blank'>
-        <img src={appleStore}/>
-      </a>
+      <div className="links">
+        <a href="https://play.google.com" target='_blank'>
+          <img src={googlePlay}/>
+        </a>
+        <a href="https://www.apple.com/ru/app-store/" target='_blank'>
+          <img src={appleStore}/>
+        </a>
+      </div>
     </div>
   </ApplicationS>
 )
